59e802696421f2cd869735287c80be52e948f59a
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-08-02 21:27:48 +0000

    add subroutes

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,10 +13,19 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Administrateur',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Distributeur',
+					autogenerate: { directory: 'guides' },
+				},
+				{
+					label: 'Boutique',
+					autogenerate: { directory: 'guides' },
 				},
 				{
 					label: 'Reference',
