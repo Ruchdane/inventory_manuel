93d411695c07c0ea6e10c004db6d8c74da08eb6e
Ruchdane AMADOU <ruchdaneabiodun@gmail.com>
2023-08-06 09:26:34 +0000

    config for github page deployement

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+  site: 'https://ruchdane.github.io',
+  base: 'inventory_manuel',
 	integrations: [
 		starlight({
 			title: 'Manuel',
